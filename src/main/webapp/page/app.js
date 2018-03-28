df9b10a3b24146094adcdf29b494ecdf479c2bd6
vukojovicic <35064939+vukojovicic@users.noreply.github.com>
2018-03-29 01:30:33 +0200

    3 stranice
    login register i userProfil stranice, index i app.js namesteni za angularisanje, treba jos angulara malo pa bi to valjda radilo

--- src/main/webapp/page/app.js
+++ src/main/webapp/page/app.js
@@ -1,4 +1,45 @@
-var apka = angular.module('apka', []);
+'use-strict';
+
+var ustanoveApp = angular.module('ustanoveApp', ['ui.router',  'angular-input-stars']);
+
+ustanoveApp.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+    $urlRouterProvider.otherwise('/login');
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            controller: 'loginController',
+            templateUrl: 'login.html',
+            data: {
+                pageTitle: 'Prijava'
+            }
+        })
+        .state('register', {
+            url: '/register',
+            controller: 'registerController',
+            templateUrl: 'register.html',
+            data: {
+                pageTitle: 'Registracija'
+            }
+        })
+        .state('userProfil', {
+            url: '/userProfil',
+            controller: 'userProfileController',
+            templateUrl: 'userProfil.html',
+            data: {
+                pageTitle: 'Moj profil'
+            }
+        })
+
+    if (!$httpProvider.defaults.headers.get) {
+        $httpProvider.defaults.headers.get = {};
+    }
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+}).run(function ($rootScope, $location, authorizationService) {
+    //nesto
+});
+
+/*var apka = angular.module('apka', []); -----------ono nase staro greota brisati
 
 apka.controller('kontro', function($scope, $http) {
     $scope.submit = function () {
@@ -11,5 +52,5 @@ apka.controller('kontro', function($scope, $http) {
         });
     };
 });
-
+*/
 
