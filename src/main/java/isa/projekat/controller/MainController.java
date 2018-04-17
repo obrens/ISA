8845e26121b461355ac2ee52d0a7f0c7e5b851e3
obrens <starovico@gmail.com>
2018-04-17 20:13:37 +0200

    Korisnika dobiti kao u MainCotnroller-u

--- src/main/java/isa/projekat/controller/MainController.java
+++ src/main/java/isa/projekat/controller/MainController.java
@@ -1,14 +1,19 @@
 package isa.projekat.controller;
 
+import isa.projekat.model.Korisnik;
 import isa.projekat.model.Ustanova;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class MainController {
-
     @RequestMapping(method = RequestMethod.GET, value = "/hello")
     public String hello() {
         System.out.println("helou");
@@ -18,7 +23,8 @@ public class MainController {
     @RequestMapping(method = RequestMethod.GET, value = "/secured/usa")
     public Ustanova usa() {
         Ustanova usaar = new Ustanova();
-        usaar.setNaziv("obika");
+        String imejl = ((Korisnik)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getImejl();
+        usaar.setNaziv(imejl);
 
         return usaar;
     }
