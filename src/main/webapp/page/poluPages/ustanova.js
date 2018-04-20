06a52849f8331f52c2bc1f3c00e5e9b09affedb0
Vuk <vukboskovic3@gmail.com>
2018-04-20 20:41:12 +0200

    Lokacija, ne radi dobro

--- src/main/webapp/page/poluPages/ustanova.js
+++ src/main/webapp/page/poluPages/ustanova.js
@@ -1,4 +1,6 @@
 ustanoveApp.controller('ustanovaController', ['$scope', '$http', '$state', '$stateParams', function ($scope, $http, $state, $stateParams) {
+
+
     $http.get('/api/ustanova/' + $stateParams.id).success(function (data) {
         $scope.ustanova = data;
     });
@@ -16,4 +18,29 @@ ustanoveApp.controller('ustanovaController', ['$scope', '$http', '$state', '$sta
         $scope.ja = data;
         $scope.nekoJe = true;
     });
+
+    var address = 'London, UK';
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        zoom: 6
+    });
+
+    var geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+            'address': address
+        },
+        function(results, status) {
+            if(status == google.maps.GeocoderStatus.OK) {
+                new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map: map
+                });
+                map.setCenter(results[0].geometry.location);
+            }
+            else {
+                // Google couldn't geocode this request. Handle appropriately.
+            }
+        });
 }]);
